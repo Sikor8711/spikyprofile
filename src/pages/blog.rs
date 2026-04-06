ceb06c92f17cb8914c5c3749d8f96170acf8f07a
Patryk Sikorski <sikor87@hotmail.com>
2026-04-06 12:43:25 +0100

    title

--- src/pages/blog.rs
+++ src/pages/blog.rs
@@ -4,7 +4,7 @@ use leptos_meta::Title;
 #[component]
 pub fn BlogPage() -> impl IntoView {
     view! {
-        <Title text="About me" />
+        <Title text="Bolg" />
         <div>
             <h1 class="text-3xl text-center">"Blog Post"</h1>
         </div>
